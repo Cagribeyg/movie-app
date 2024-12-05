744dc1e080c19533355042973b4a4641111cfb55
Cagribeyg <cagribeyg@gmail.com>
2024-12-05 10:58:52 +0300

    feat: colors and fonts transformed with scss

--- src/components/MovieTable/MovieTable.tsx
+++ src/components/MovieTable/MovieTable.tsx
@@ -46,6 +46,14 @@ const MovieTable = ({
             },
           }}
           onRowClick={handleMovieClick}
+          sx={{
+            boxShadow: 2,
+            border: 2,
+            borderColor: "primary.light",
+            "& .MuiDataGrid-cell:hover": {
+              color: "primary.main",
+            },
+          }}
         />
       </Box>
     </div>
